f3692ccc699b8fca33c8af7085e43dea87cafd7a
Wang Yuanhui <MinasWang@users.noreply.github.com>
2019-10-12 17:00:41 +0800

    Create zykey.go

---
+++ zykey/zykey.go
@@ -0,0 +1,44 @@
+package zykey
+
+import (
+    "net/http"
+    "time"
+    "io/ioutil"
+    "math/rand"
+    "io"
+    "os"
+    "strconv"
+)
+
+
+
+func GetKey(url string ) (int, error) {
+	
+    rand.Seed(time.Now().Unix())
+    tmp_file := ".li_" + strconv.Itoa(rand.Intn(1000000))
+    out, err := os.Create(tmp_file)
+    if err != nil {
+        return 0, err
+    }
+    defer os.Remove(tmp_file)
+    defer out.Close()
+
+    res, err := http.Get(url)
+    if err != nil {
+        return 0, err
+    }
+    defer res.Body.Close()
+
+    _, err = io.Copy(out, res.Body)
+    if err != nil {
+        return 0, err
+    }
+    contents, err := ioutil.ReadFile(tmp_file); 
+    if err == nil{
+        val, _  :=  strconv.Atoi(string(contents))
+        return val, nil
+    } else {
+    	return 0, err
+    }
+}
+
